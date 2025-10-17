684b2c735a5e189178fb5b594fed723fc5e9c840
Explorer16 <asadullaevdonier@gmail.com>
2025-10-17 23:05:03 +0500

    Start

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-teal-600 text-lg">
+        <label>aaaaaaaaaфыыыыыыыыaaaaaaaaaaaaaaaaaaaaaa</label>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
