5603a8c5a6a257fbe8b4d24740cd139032699bf6
Explorer16 <asadullaevdonier@gmail.com>
2025-10-18 15:59:46 +0500

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(
+    [
+        'prefix' => 'api',
+        'namespace' => 'App\\Http\\Controllers\\Api\\'
+    ], function () {
+        Route::apiResource('records', 'RecordController');
+        Route::apiResource('record_categories', 'RecordCategoryController');
+    }
+);
