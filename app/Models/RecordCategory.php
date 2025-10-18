58294d6157964c566eebee9378820cedd206b38e
Explorer16 <asadullaevdonier@gmail.com>
2025-10-18 22:03:10 +0500

    Added scopes in record categories

--- app/Models/RecordCategory.php
+++ app/Models/RecordCategory.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class RecordCategory extends Model
 {
     protected $fillable = ['name', 'type'];
+
+    public function scopeSort($query)
+    {
+        return $query->orderBy(request()->input('column', 'id'), request()->input('direction', 'desc'));
+    }
+
+    public function scopeFilter($query)
+    {
+        if ($filter = request('name')) {
+            $query = $query->where('name', 'ilike', '%' . $filter . '%');
+        }
+        if ($filter = request('type')) {
+            $query = $query->where('type', $filter);
+        }
+
+        return $query;
+    }
 }
