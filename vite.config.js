5da1a9088b00de713261390c36652d86502c8edc
Explorer16 <asadullaevdonier@gmail.com>
2025-10-19 17:42:47 +0500

    added server params

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,17 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+            port: 5173,
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
